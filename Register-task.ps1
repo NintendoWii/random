a3d1915df56abdb3f8eaa60ea41dc5efa5445830
NintendoWii <kyle.s.desjardins1@gmail.com>
2024-06-22 10:38:25 -0400

    Add files via upload

---
+++ Register-task.ps1
@@ -0,0 +1,4 @@
+﻿$Trigger = New-ScheduledTaskTrigger -At 9:00am –Daily
+$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "$env:USERPROFILE\Desktop\ZeroTier_BD\ZT_BD.ps1"
+Register-ScheduledTask -TaskName "ZT_BD" -Action $action -Trigger $trigger -Principal $principal
